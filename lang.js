b015aa3f0e91ae8475c81412b23dfb54d6079c2a
Anton <101133666+theSameAnton@users.noreply.github.com>
2022-03-13 10:44:14 +0100

    Add files via upload

---
+++ lang.js
@@ -0,0 +1,101 @@
+'use strict'
+
+const langArr = {
+    "parallax-text" :  {
+        "en": "What's about skills?",
+        "cz": "Co se týká znalosti?",
+    },
+    "info-text" :  {
+        "en": "Anton, 20 years old. I live and study in Prague. Saying that I am a responsible and ambitious student who wants to advance in all areas related to the Web is, most probably, pointless. But I will prove my skills in practice. What I can say for sure is that I’m not afraid of making mistakes. Standing still and not developing is much worse. That is why I’m always looking for new and reliable, up-to-date information! In addition to writing codes on a regular basis, I also attend certain events and meetups, courses, and lectures related to development and design, which one can find freely available on the Internet. An interesting fact about me is that I am self-taught. But I don’t think it will cause trouble. On the contrary, I think that this approach has many advantages. Firstly, it is independence, which I have developed over the years. Secondly, if there’s a problem, I don’t need to ask for help from a mentor. I managed to master an important skill for my profession – «the ability to google»",
+        "cz": "Anton. 20 let. Bydlím a studují v Praze. Pravděpodobně, když vám řeknu, že jsem cílevědomý a zodpovědný student, který chce dosáhnout ve všech oblastech, co se týká webu, tak to pro Vás asi nic znamenat nebude. Ale zkusím Vám to dokázat v praxi. Podle mého názoru, když stojíte na jednom místě, tak je to mnohem horší, než dělat chyby. A hlavně vždycky hledám novou, hlavně ověřenou informaci. Kromě pravidelného psaní kódů, navštěvuji meetupy, kurzy a lekce, které se týkají programování a designu. \n" +
+            "Jeden zajímavý fakt o mně, že skoro vždycky jsem měl samostudium. Jaké má plusy tento přístup? Zaprvé, to je samostatnost, kterou jsem rozvíjel roky. Zadruhé, jestli mám nějaký problém, tak nemusím běžet k mentorovi a prosit o pomoc. Dokázal jsem získat důležitou schopnost mé profese - «schopnost googlit (nebo-li používat Google)».\n",
+    },
+    "info-h2" :  {
+        "en": "about me",
+        "cz": "o mně",
+    },
+    "info-h2-2" :  {
+        "en": "education",
+        "cz": "vzdělání",
+    },
+    "contacts-h2" : {
+        "en": "Contacts",
+        "cz": "Kontakty"
+    },
+    "contacts-h2-2" : {
+        "en": "Connect me!",
+        "cz": "Kontaktujte me!"
+    },
+    "contacts-text" : {
+        "en": "If you need Front-end developer in your team or for making a new project, so let me know!",
+        "cz": "Pokud chcete mít Front-end developera do svého týmu nebo-li vytvořit nový projekt - dejte mi vědět!"
+    },
+    "contacts-h4" : {
+        "en": "Phone",
+        "cz": "Telefon"
+    },
+    "contacts-h4-2" : {
+        "en": "social media",
+        "cz": "sociální media"
+    },
+    "form-1" : {
+        "en": "Name:",
+        "cz": "Jméno:"
+    },
+    "form-2" : {
+        "en": "Message:",
+        "cz": "Zprávy:"
+    },
+    "button" : {
+        "en": "Submit",
+        "cz": "Odeslat"
+    },
+    "footer" : {
+        "en": "I'm a Front-end developer, I can create some websites (use for it: HTML5, CSS3, JS etc.)",
+        "cz": "Jsem webový vyvojář, který může vytvořit různé webové strnáky(používám k tomu HTML5, CSS3, JS atd."
+    },
+    "info-p" : {
+        "en": "2020 - now",
+        "cz": "2020 - doted'"
+    },
+    "info-p-2" : {
+        "en": "Czech University of Life Sciences Prague (CZU)",
+        "cz": "Česká Zemědělská Univerzita (ČZU)"
+    },
+    "info-p-3" : {
+        "en": "Study program: Informatics",
+        "cz": "Studijný program: Informatika"
+    },
+    "cv" : {
+        "en": "Download CV",
+        "cz": "Stahnout CV"
+    },
+    "link-1" : {
+        "en": "Home",
+        "cz": "Domu"
+    },
+    "link-2" : {
+        "en": "About Me",
+        "cz": "O mně"
+    },
+    "link-3" : {
+        "en": "Skills",
+        "cz": "Znalosti"
+    },
+    "link-4" : {
+        "en": "Contacts",
+        "cz": "Kontakty"
+    },
+    "an-1" : {
+        "en": "Hi, my name is Anton.",
+        "cz": "Zdravím,  jsem  Anton."
+    },
+    "an-2" : {
+        "en": "I'm a Front-end Developer.",
+        "cz": "Delám webového developera."
+    },
+    "an-3" : {
+        "en": "And this is my Portfolio Website!",
+        "cz": "Vítám vás na své webové strance!"
+    },
+}
